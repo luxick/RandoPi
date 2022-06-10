ebee7989b861d6a4cf457e9aa653be0069b9a4f4
luxick <git@luxick.de>
2022-06-11 00:07:43 +0200

    Add keyboard shortcuts

--- RandoPi.Desktop/MainWindow.cs
+++ RandoPi.Desktop/MainWindow.cs
@@ -1,12 +1,13 @@
 using System;
 using Gtk;
-using RandoPi.Shared;
+using Key = Gdk.Key;
 using UI = Gtk.Builder.ObjectAttribute;
 
 namespace RandoPi.Desktop
 {
     class MainWindow : Window
     {
+        private bool _isFullScreen;
         [UI] private Label _label1 = null;
         [UI] private Button _button1 = null;
 
@@ -20,15 +21,38 @@ namespace RandoPi.Desktop
         {
             builder.Autoconnect(this);
 
+            KeyReleaseEvent += OnKeyReleaseEvent;
             DeleteEvent += Window_DeleteEvent;
             _button1.Clicked += Button1_Clicked;
+        }
+
+        private void OnKeyReleaseEvent(object o, KeyReleaseEventArgs args)
+        {
+            if (args.Event.Key is Key.q or Key.Escape)
+                Application.Quit();
+
+            if (args.Event.Key is Key.f)
+                ToogleFullscreen();
+
+        }
+
+        private void ToogleFullscreen()
+        {
+            if (_isFullScreen)
+            {
+                Unfullscreen();
+                _isFullScreen = false;
+            }
+            else
+            {
+                Fullscreen();
+                _isFullScreen = true;
+            }
         }
 
         private void Window_DeleteEvent(object sender, DeleteEventArgs a)
         {
             Application.Quit();
-            // Top the API server
-            // StaticData.ApiServerWorker.Kill(true);
         }
 
         private void Button1_Clicked(object sender, EventArgs a)
