ca117b447992cd2f2c63e9f22af0b516795d70cc
luxick <git@luxick.de>
2022-06-11 15:06:47 +0200

    add image display

--- RandoPi.Desktop/MainWindow.cs
+++ RandoPi.Desktop/MainWindow.cs
@@ -1,64 +1,84 @@
 using System;
+using System.Diagnostics;
+using System.IO;
 using Gtk;
+using Cairo;
+using Gdk;
 using Key = Gdk.Key;
 using UI = Gtk.Builder.ObjectAttribute;
+using Window = Gtk.Window;
+using WindowType = Gtk.WindowType;
 
-namespace RandoPi.Desktop
+namespace RandoPi.Desktop;
+
+class MainWindow : Window
 {
-    class MainWindow : Window
+    private bool _isFullScreen;
+
+    private Pixbuf _currentFrame;
+        
+    public MainWindow() : base(WindowType.Toplevel)
     {
-        private bool _isFullScreen;
-        [UI] private Label _label1 = null;
-        [UI] private Button _button1 = null;
+        KeyReleaseEvent += OnKeyReleaseEvent;
+        DeleteEvent += Window_DeleteEvent;
+        
+        var drawingArea = new DrawingArea();
+        drawingArea.Drawn += AreaDrawn;
+        Add(drawingArea);
+        
+        LoadNextFrame();
 
-        private int _counter;
+        ShowAll();
+        
+        if (!Debugger.IsAttached)
+            ToggleFullscreen();
+    }
 
-        public MainWindow() : this(new Builder("MainWindow.glade"))
-        {
-        }
+    private void LoadNextFrame()
+    {
+        var bytes = File.ReadAllBytes(@"image.png");
+        _currentFrame = new Pixbuf(bytes);
+    }
 
-        private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
-        {
-            builder.Autoconnect(this);
+    private void AreaDrawn(object o, DrawnArgs args)
+    {
+        var cr = args.Cr;
+        cr.SetSourceRGB(0, 0, 0);
+        cr.Paint();
+        
+        Gdk.CairoHelper.SetSourcePixbuf(cr, _currentFrame, 0, 0);
+        cr.Paint();
+        
+        ((IDisposable)cr.GetTarget()).Dispose();
+        ((IDisposable)cr).Dispose();
+    }
 
-            KeyReleaseEvent += OnKeyReleaseEvent;
-            DeleteEvent += Window_DeleteEvent;
-            _button1.Clicked += Button1_Clicked;
-        }
-
-        private void OnKeyReleaseEvent(object o, KeyReleaseEventArgs args)
-        {
-            if (args.Event.Key is Key.q or Key.Escape)
-                Application.Quit();
-
-            if (args.Event.Key is Key.f)
-                ToogleFullscreen();
-
-        }
-
-        private void ToogleFullscreen()
-        {
-            if (_isFullScreen)
-            {
-                Unfullscreen();
-                _isFullScreen = false;
-            }
-            else
-            {
-                Fullscreen();
-                _isFullScreen = true;
-            }
-        }
-
-        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
-        {
+    private void OnKeyReleaseEvent(object o, KeyReleaseEventArgs args)
+    {
+        if (args.Event.Key is Key.q or Key.Escape)
             Application.Quit();
-        }
 
-        private void Button1_Clicked(object sender, EventArgs a)
+        if (args.Event.Key is Key.f)
+            ToggleFullscreen();
+
+    }
+
+    private void ToggleFullscreen()
+    {
+        if (_isFullScreen)
         {
-            _counter++;
-            _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
+            Unfullscreen();
+            _isFullScreen = false;
         }
+        else
+        {
+            Fullscreen();
+            _isFullScreen = true;
+        }
+    }
+
+    private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
     }
 }
