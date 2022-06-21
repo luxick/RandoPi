d8c53e64afc2ff8f16f8572b011d26df545637f4
luxick <git@luxick.de>
2022-06-21 21:18:26 +0200

    First attempt at OSD

---
+++ RandoPi.Desktop/Helpers/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Gtk;
+
+namespace RandoPi.Desktop.Helpers;
+
+public static class Extensions
+{
+    public static void TryRemove(this Container container, Widget widget)
+    {
+        if (container.HasChild(widget))
+            container.Remove(widget);
+    }
+    
+    public static void TryAdd(this Container container, Widget widget)
+    {
+        if (container.HasChild(widget))
+            return;
+        container.Add(widget);
+    }
+    public static bool HasChild(this Container container, Widget widget)
+        => container.Children.FirstOrDefault(x => ReferenceEquals(x, widget)) != null;
+}
--- RandoPi.Desktop/MainWindow.cs
+++ RandoPi.Desktop/MainWindow.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Timers;
 using Gtk;
 using Gdk;
+using RandoPi.Desktop.Helpers;
 using RandoPi.Desktop.Providers;
 using RandoPi.Shared;
 using Key = Gdk.Key;
@@ -14,25 +16,61 @@ namespace RandoPi.Desktop;
 
 class MainWindow : Window
 {
+    private const double OSDTimeout = 3000;
+    
     private bool _isFullScreen;
     private Pixbuf? _currentFrame;
     private readonly ImageProvider _imageProvider;
-    private DrawingArea _drawingArea;
+    private readonly DrawingArea _drawingArea;
+    private readonly Box _overlayBox;
+    private readonly Box _osdInfo;
+    private readonly Label _osdText;
+    private readonly Overlay _overlay;
+    private readonly Spinner _spinner;
+
+    private readonly Timer _osdTimer;
         
     public MainWindow() : base(WindowType.Toplevel)
     {
         _imageProvider = new ImageProvider();
         _imageProvider.LoadImageSources();
-        _imageProvider.CurrentMode = ImageMode.Foxes;
+        _imageProvider.CurrentMode = ImageMode.File;
         
         KeyReleaseEvent += OnKeyReleaseEvent;
         DeleteEvent += Window_DeleteEvent;
         
         _drawingArea = new DrawingArea();
         _drawingArea.Drawn += AreaDrawn;
-        Add(_drawingArea);
 
+        _overlayBox = new Box(Orientation.Vertical, 0);
+        _overlayBox.Expand = true;
+
+        _spinner = new Spinner { Active = true };
+        
+        _osdText= new Label();
+        
+        _osdInfo = new Box(Orientation.Horizontal, 10);
+        _osdInfo.Add(_osdText);
+        _osdInfo.Add(_spinner);
+        
+        var box = new Box(Orientation.Horizontal, 0);
+        box.PackEnd(_osdInfo, false, true, 0);
+
+        _overlayBox.Add(new Label("This is the OSD"));
+        _overlayBox.PackEnd(box, false, true, 0);
+
+        _overlay = new Overlay();
+        _overlay.Child = _drawingArea;
+        Add(_overlay);
         ShowAll();
+
+        _osdTimer = new Timer();
+        _osdTimer.Interval = OSDTimeout;
+        _osdTimer.Elapsed += (_, _) =>
+        {
+            HideOSD();
+            _osdTimer.Enabled = false;
+        };
         
         if (!Debugger.IsAttached)
             ToggleFullscreen();
@@ -43,6 +81,7 @@ class MainWindow : Window
         var result = _imageProvider.LoadNextImage();
         if (!result.Success) return;
         _currentFrame = new Pixbuf(result.Value);
+        DisplayOnOSD("Next Image Is Loaded!");
         QueueDraw();
     }
 
@@ -67,15 +106,54 @@ class MainWindow : Window
 
     private void OnKeyReleaseEvent(object o, KeyReleaseEventArgs args)
     {
-        if (args.Event.Key is Key.q or Key.Escape)
-            Application.Quit();
+        switch (args.Event.Key)
+        {
+            case Key.q or Key.Escape:
+                Application.Quit();
+                break;
+            case Key.f:
+                ToggleFullscreen();
+                break;
+            case Key.space:
+                PrepareNextFrame();
+                break;
+            case Key.Key_1:
+                ShowOSD();
+                break;
+            case Key.Key_2:
+                HideOSD();
+                break;
+            case Key.Key_3:
+                DisplayOnOSD("Test Message", true);
+                break;
+        }
+    }
 
-        if (args.Event.Key is Key.f)
-            ToggleFullscreen();
+    private void DisplayOnOSD(string message, bool spin = false)
+    {
+        
+        _osdText.Text = message;
+        ShowOSD();
 
-        if (args.Event.Key is Key.space)
-            PrepareNextFrame();
+        // if (spin)
+        //     _osdInfo.TryAdd(_spinner);
+        // else
+        //     _osdInfo.TryRemove(_spinner);
+    }
 
+    private void ShowOSD()
+    {
+        if (_overlay.Children.Length == 1)
+        {
+            _overlay.AddOverlay(_overlayBox);
+            _overlay.ShowAll();
+            _osdTimer.Enabled = true;
+        }
+    }
+
+    private void HideOSD()
+    {
+        if (_overlay.Children.Length == 2) _overlay.TryRemove(_overlayBox);
     }
 
     private void ToggleFullscreen()
